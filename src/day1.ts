3ebcaec405fc03fb8f9585e33016670f4d75370b
Gabriel Plassard <gabriel.plassard@gmail.com>
2020-01-01 18:44:02 +0100

    day 1 / part 2

--- src/day1.ts
+++ src/day1.ts
@@ -1,10 +1,20 @@
 import * as fs from "fs";
 
+function computeFuel(mass: number): number {
+    const amount = Math.floor(mass / 3) - 2;
+    if (amount < 0) {
+        return 0;
+    }
+    return amount + computeFuel(amount);
+}
+
+
 const result = fs
     .readFileSync('inputs/day1.txt').toString()
     .split("\n")
     .filter(l => l.length > 0)
-    .map(i => Math.floor(parseFloat(i) / 3) - 2)
+    .map(parseFloat)
+    .map(computeFuel)
     .reduce((a, b) => a + b, 0);
 
 console.log(result);
